d676d6ef689ff839bd573acb4f5fa879510e0841
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-08-13 22:56:39 +1000

    created search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
 class SearchController < ApplicationController
-  # Mocking API integration for React page
   def find
-    @data = [{
-      date: '12/12/2019',
-      flight: '050',
+    @data = Flight.where(
       origin: params[:origin],
-      destination: params[:destination],
-      airplane: '150'
-
-    }]
+      destination: params[:destination]
+    )
     render json: @data
   end
 end
