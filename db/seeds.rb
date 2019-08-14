72e19daba1241a0c953b681bfcf0d843e656cfbf
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-08-14 16:16:57 +1000

    changes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,26 +8,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 User.destroy_all
 puts 'Creating User'
-u1 = User.create :name => 'Nilana', :email => 'nilana@ga.co'
-u2 = User.create :name => 'Victor', :email => 'victor@ga.co'
-u3 = User.create :name => 'Paulina', :email => 'paulina@ga.co'
+u1 = User.create name: 'Nilana', email: 'nilana@ga.co'
+u2 = User.create name: 'Victor', email: 'victor@ga.co'
+u3 = User.create name: 'Paulina', email: 'paulina@ga.co'
 
 Airplane.destroy_all
 puts 'Creating Airplane'
-a1 = Airplane.create :planeNo => 757, :columns => 'A', row: 20
-a2 = Airplane.create :planeNo => 800, :columns => 'A', row: 28
-a3 = Airplane.create :planeNo => 444, :columns => 'A', row: 50
+a1 = Airplane.create planeNo: 757, columns: 'A', row: 20
+a2 = Airplane.create planeNo: 800, columns: 'A', row: 28
+a3 = Airplane.create planeNo: 444, columns: 'A', row: 50
 
 Flight.destroy_all
 puts 'Creating Flights'
-f1 = Flight.create :origin => 'SYD', :destination => 'SNG'
-f2 = Flight.create :origin => 'MAL', :destination => 'SNG'
-f3 = Flight.create :origin => 'SNG', :destination => 'PER'
+f1 = Flight.create origin: 'SYD', destination: 'SNG', dateTime: '2019-12-20'
+f2 = Flight.create origin: 'MAL', destination: 'SNG', dateTime: '2019-11-22'
+f3 = Flight.create origin: 'SNG', destination: 'PER', dateTime: '2019-10-21'
 
-puts "associations"
+puts 'associations'
 u1.flights << f1
 
 a1.flights << f3
